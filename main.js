97544376ab35fb9f4c8c77c28f93666bbc860a7f
Marsel Atniashev <atn@marsel.name>
2019-11-14 17:35:29 +0500

    Add autostart, forbid closing, add tray icon

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Tray } = require('electron')
+const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let tr
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {},
     autoHideMenuBar: true,
+    // show: false, // turn on for .ge
   })
 
-  const webPlayerURL = 'https://pixelart-web.netlify.com' // 'https://pixelart-web.netlify.com'
+  const iconPath = path.join(__dirname, 'icon.ico')
+  tr = new Tray(iconPath)
+  tr.addListener('click', () => {
+    show()
+  })
+
+  const webPlayerURL = 'https://pixelart-web.netlify.com'
   mainWindow.loadURL(webPlayerURL)
 
   mainWindow.webContents.on('dom-ready', () => {
     // we can't just set BrowserWindow.setFullscreen(true) because HTML5 fullscreen API will stop working
-    mainWindow.webContents.executeJavaScript('document.documentElement.requestFullscreen()', true)
-  });
+    mainWindow.webContents.executeJavaScript(
+      'document.documentElement.requestFullscreen()',
+      true
+    )
+  })
 
-  mainWindow.webContents.on('crashed', (e) => {
+  mainWindow.webContents.on('crashed', e => {
     console.error(e)
     reloadWebPlayer()
-  });
+  })
 
-  mainWindow.webContents.on('unresponsive', (e) => {
+  mainWindow.webContents.on('unresponsive', e => {
     console.error(e)
     reloadWebPlayer()
-  });
+  })
 
   function reloadWebPlayer() {
     app.relaunch()
@@ -37,17 +49,46 @@ function createWindow () {
 
   // mainWindow.webContents.openDevTools()
 
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     mainWindow = null
   })
+
+  mainWindow.on('close', e => {
+    e.preventDefault()
+    mainWindow.hide();
+  });
 }
+
+// autorun
+app.setLoginItemSettings({
+  openAtLogin: true,
+});
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   if (mainWindow === null) createWindow()
 })
+
+// single app
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    show()
+  })
+}
+
+function show() {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+    mainWindow.show()
+  }
+}
