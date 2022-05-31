d94c3adf0a81faa2858887011f343fcaccfc1123
Ilya Igonkin <osdev0@gmail.com>
2022-05-31 17:51:51 +0400

    Implement sending Windows App version as Player Info (#6)

--- preload.js
+++ preload.js
@@ -1,9 +1,16 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const { contextBridge, ipcRenderer } = require('electron')
+let version;
+
+ipcRenderer.invoke('getVersion').then(v => version = v);
 
 contextBridge.exposeInMainWorld('FugoElectronBridge', {
   doScreenshot(url) {
     ipcRenderer.send('doScreenshot', url);
+  },
+
+  getVersion() {
+    return version;
   }
 })
