3ec25977d1935af3110e070ccac63ffe0fafbfff
Marsel Atniashev <atn@marsel.name>
2023-04-11 17:57:15 +0500

    wip

---
+++ src/main.ts
@@ -0,0 +1,257 @@
+import { NativeImage } from 'electron'
+
+const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron')
+const path = require('path')
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log')
+const fetch = require('node-fetch')
+
+log.transports.file.level = 'debug'
+autoUpdater.logger = log
+let shouldQuitForUpdate = false
+autoUpdater.on('update-downloaded', () => {
+  shouldQuitForUpdate = true
+  autoUpdater.quitAndInstall()
+})
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow: Electron.BrowserWindow | null = null
+let tr
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      autoplayPolicy: 'no-user-gesture-required',
+      nodeIntegration: true,
+    },
+    autoHideMenuBar: true,
+    kiosk: app.isPackaged,
+    alwaysOnTop: true,
+    // show: false, // turn on for .ge
+  })
+  console.log('hm')
+
+  const iconPath = path.join(__dirname, '../icon.png')
+  tr = new Tray(iconPath)
+  tr.addListener('click', () => {
+    show()
+  })
+  tr.setContextMenu(
+    Menu.buildFromTemplate([
+      {
+        label: 'About',
+        role: 'about',
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.exit(0)
+        },
+      },
+    ])
+  )
+
+  console.log('before load url')
+  const webPlayerURL = 'http://localhost:3001/'
+  mainWindow.loadURL(webPlayerURL)
+  console.log('load url')
+
+  mainWindow.webContents.on('dom-ready', () => {
+    // we can't just set BrowserWindow.setFullscreen(true) because HTML5 fullscreen API will stop working
+    mainWindow.webContents.executeJavaScript(
+      'document.documentElement.requestFullscreen()',
+      true
+    )
+  })
+
+  // set safe guards after a while to avoid infinite reload
+  setTimeout(() => setSafeGuards(mainWindow, app), 1000 * 60 * 10)
+
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  mainWindow.on('close', (e: any) => {
+    if (!shouldQuitForUpdate) {
+      e.preventDefault()
+    }
+  })
+
+  ipcMain.on('doScreenshot', handleDoScreenshot)
+  ipcMain.handle('getVersion', handleGetVersion)
+
+  ipcMain.handle('prepareWebsite', handlePrepareWebsite)
+  ipcMain.handle('displayWebsite', handleDisplayWebsite)
+  ipcMain.handle('destroyWebsite', handleDestroyWebsite)
+  ipcMain.handle('prepareWebsiteFullscreen', handlePrepareWebsiteFullscreen)
+  ipcMain.handle('displayWebsiteFullscreen', handleDisplayWebsiteFullscreen)
+  ipcMain.handle('destroyWebsiteFullscreen', handleDestroyWebsiteFullscreen)
+
+  autoUpdater.checkForUpdatesAndNotify()
+}
+
+let preparingFullscreenWebsiteId = ''
+let displayingFullscreenWebsiteId = ''
+let displayWebsites: { [keys: string]: Electron.BrowserWindow } = {}
+function handlePrepareWebsite(
+  event: any,
+  url: string,
+  code: string,
+  orientation: number,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  id: string
+) {
+  handlePrepareWebsiteFullscreen(event, url, code, orientation)
+}
+
+function handleDisplayWebsite(event: any, id: string) {
+  handleDisplayWebsiteFullscreen(event)
+}
+
+function handleDestroyWebsite(event: any, id: string) {
+  handleDestroyWebsiteFullscreen(event)
+}
+
+function handlePrepareWebsiteFullscreen(
+  _event: any,
+  url: string,
+  code: string,
+  orientation: number
+) {
+  console.log('handlePrepareWebsiteFullscreen')
+  if (preparingFullscreenWebsiteId) {
+    handleDestroyWebsiteFullscreen(_event, preparingFullscreenWebsiteId)
+  }
+
+  const id = Math.random().toString()
+  preparingFullscreenWebsiteId = id
+  console.log('preparing ' + id)
+
+  displayWebsites[id] = new BrowserWindow({
+    // width: 800,
+    // height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      autoplayPolicy: 'no-user-gesture-required',
+      nodeIntegration: true,
+    },
+    autoHideMenuBar: true,
+    kiosk: app.isPackaged,
+    alwaysOnTop: true,
+    show: false,
+    fullscreen: true,
+  })
+  displayWebsites[id].loadURL(url)
+
+  displayWebsites[id].webContents.on('did-finish-load', () => {
+    console.log('executeJavaScript')
+    displayWebsites[id].webContents.executeJavaScript(code)
+  })
+}
+
+function handleDisplayWebsiteFullscreen(_event: any) {
+  console.log('handleDisplayWebsiteFullscreen' + preparingFullscreenWebsiteId)
+  displayingFullscreenWebsiteId = preparingFullscreenWebsiteId
+  preparingFullscreenWebsiteId = ''
+  displayWebsites[displayingFullscreenWebsiteId].show()
+}
+
+function handleDestroyWebsiteFullscreen(_event: any, id: string = '') {
+  if (!id) {
+    id = displayingFullscreenWebsiteId
+    displayingFullscreenWebsiteId = ''
+  }
+  console.log('destroy ' + id)
+  displayWebsites[id].hide()
+  displayWebsites[id].close()
+  displayWebsites[id].destroy()
+  displayWebsites[id] = null
+  delete displayWebsites[id]
+
+  if (!preparingFullscreenWebsiteId) {
+    mainWindow.focus()
+  }
+}
+
+// autorun
+app.setLoginItemSettings({
+  openAtLogin: true,
+})
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
+// single app
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    show()
+  })
+}
+
+function show() {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+    mainWindow.show()
+  }
+}
+
+// setup guards that will restart the app in case of freezed web player
+function setSafeGuards(
+  mainWindow: Electron.BrowserWindow,
+  appGuarded: typeof app
+) {
+  mainWindow.webContents.on('crashed', (e: any) => {
+    console.error(e)
+    reloadWebPlayer(appGuarded)
+  })
+
+  mainWindow.webContents.on('unresponsive', (e: any) => {
+    console.error(e)
+    reloadWebPlayer(appGuarded)
+  })
+
+  console.log('Safe guards are set')
+}
+
+function reloadWebPlayer(appGuarded: typeof app) {
+  appGuarded.relaunch()
+  appGuarded.exit(0)
+}
+
+function handleDoScreenshot(event: any, url: string) {
+  mainWindow.webContents.capturePage().then((image: NativeImage) => {
+    fetch(url, {
+      method: 'PUT',
+      body: image.toJPEG(75),
+    })
+  })
+}
+
+function handleGetVersion() {
+  return app.getVersion()
+}
--- src/preload.ts
+++ src/preload.ts
@@ -1,21 +1,20 @@
-alert('HIA')
-
 // All of the Node.js APIs are available in the preload process.
-import { BrowserWindow } from 'electron'
+// const { BrowserWindow } = require('electron')
 
 // It has the same sandbox as a Chrome extension.
 const { contextBridge, ipcRenderer } = require('electron')
-let version
+let version: string = ''
 
 ipcRenderer.invoke('getVersion').then((v) => (version = v))
 
 const websites = {}
-const fugoElectronBridge: FugoElectronBridge = {
+let fugoElectronBridgeInstance: FugoElectronBridge = {
   doScreenshot(url) {
     ipcRenderer.send('doScreenshot', url)
   },
 
   getVersion() {
+    console.log('VERSION')
     return version
   },
 
@@ -29,43 +28,46 @@ const fugoElectronBridge: FugoElectronBridge = {
     height: number,
     id: string
   ) {
-    this.prepareWebsiteFullscreen(url, code, orientation)
-  },
-
-  prepareWebsiteFullscreen(url: string, code: string, orientation: number) {
-    const id = 'fullscreen'
-    websites[id] = new BrowserWindow({
-      webPreferences: { offscreen: true },
-      show: false,
-    })
-    websites[id].loadURL(url)
-
-    websites[id].webContents.on('did-finish-load', () => {
-      websites[id].webContents.executeJavaScript(code)
-    })
-  },
-
-  displayWebsiteFullscreen() {
-    const id = 'fullscreen'
-    websites[id].show()
+    ipcRenderer.invoke(
+      'prepareWebsite',
+      url,
+      code,
+      orientation,
+      x,
+      y,
+      width,
+      height,
+      id
+    )
   },
 
   displayWebsite(id: string) {
-    this.displayWebsiteFullscreen()
-  },
-
-  destroyWebsiteFullscreen() {
-    const id = 'fullscreen'
-    websites[id].destroy()
+    ipcRenderer.invoke('displayWebsite', id)
   },
 
   destroyWebsite(id: string) {
-    this.destroyWebsiteFullscreen()
+    ipcRenderer.invoke('destroyWebsite', id)
+  },
+
+  prepareWebsiteFullscreen(url: string, code: string, orientation: number) {
+    console.log('ipcRenderer.invoke prepareWebsiteFullscreen')
+    ipcRenderer.invoke('prepareWebsiteFullscreen', url, code, orientation)
+  },
+
+  displayWebsiteFullscreen() {
+    ipcRenderer.invoke('displayWebsiteFullscreen')
+  },
+
+  destroyWebsiteFullscreen() {
+    ipcRenderer.invoke('destroyWebsiteFullscreen')
   },
 }
 
-contextBridge.exposeInMainWorld('FugoElectronBridge', fugoElectronBridge)
-debugger
+contextBridge.exposeInMainWorld(
+  'FugoElectronBridge',
+  fugoElectronBridgeInstance
+)
+
 console.log(
   '%c FugoElectronBridge is exposed to the window object',
   'color: green; font-size: 20px;'
