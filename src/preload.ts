0aedba5fe0dcd4aa4bc9936face4149d5a71334b
Marsel Atniashev <atn@marsel.name>
2023-04-14 16:55:12 +0500

    Add support for display website API
    This allows Windows users to write scripts to display their websites.

--- src/preload.ts
+++ src/preload.ts
@@ -1,16 +1,97 @@
 // All of the Node.js APIs are available in the preload process.
+// const { BrowserWindow } = require('electron')
+
 // It has the same sandbox as a Chrome extension.
 const { contextBridge, ipcRenderer } = require('electron')
-let version;
+let version: string = ''
 
-ipcRenderer.invoke('getVersion').then(v => version = v);
+ipcRenderer.invoke('getVersion').then((v) => (version = v))
 
-contextBridge.exposeInMainWorld('FugoElectronBridge', {
+const websites = {}
+let fugoElectronBridgeInstance: FugoElectronBridge = {
   doScreenshot(url) {
-    ipcRenderer.send('doScreenshot', url);
+    ipcRenderer.send('doScreenshot', url)
   },
 
   getVersion() {
-    return version;
-  }
-})
+    console.log('VERSION')
+    return version
+  },
+
+  prepareWebsite(
+    url: string,
+    code: string,
+    orientation: number,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    id: string
+  ) {
+    ipcRenderer.invoke(
+      'prepareWebsite',
+      url,
+      code,
+      orientation,
+      x,
+      y,
+      width,
+      height,
+      id
+    )
+  },
+
+  displayWebsite(id: string) {
+    ipcRenderer.invoke('displayWebsite', id)
+  },
+
+  destroyWebsite(id: string) {
+    ipcRenderer.invoke('destroyWebsite', id)
+  },
+
+  prepareWebsiteFullscreen(url: string, code: string, orientation: number) {
+    console.log('ipcRenderer.invoke prepareWebsiteFullscreen')
+    ipcRenderer.invoke('prepareWebsiteFullscreen', url, code, orientation)
+  },
+
+  displayWebsiteFullscreen() {
+    ipcRenderer.invoke('displayWebsiteFullscreen')
+  },
+
+  destroyWebsiteFullscreen() {
+    ipcRenderer.invoke('destroyWebsiteFullscreen')
+  },
+}
+
+contextBridge.exposeInMainWorld(
+  'FugoElectronBridge',
+  fugoElectronBridgeInstance
+)
+
+console.log(
+  '%c FugoElectronBridge is exposed to the window object',
+  'color: green; font-size: 20px;'
+)
+
+type FugoElectronBridge = {
+  getVersion: () => string
+  doScreenshot(url: string): void
+} & DisplayWebsite
+
+interface DisplayWebsite {
+  prepareWebsite(
+    url: string,
+    code: string,
+    orientation: number,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    id: string
+  ): void
+  prepareWebsiteFullscreen(url: string, code: string, orientation: number): void
+  displayWebsiteFullscreen: () => void
+  displayWebsite: (id: string) => void
+  destroyWebsiteFullscreen: () => void
+  destroyWebsite: (id: string) => void
+}
